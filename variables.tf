30d51589e6ac26f36798ef19c7dfc5d65127aba1
Vedant Pareek <dunefro@gmail.com>
2024-10-22 19:10:17 +0530

    terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,51 +1,51 @@
 variable "chart_name" {
-  type = string
+  type        = string
   description = "Name of the chart"
 }
 
 variable "chart_version" {
-  type = string
+  type        = string
   description = "Version of the chart"
 }
 
 variable "release_name" {
-  type = string
+  type        = string
   description = "Release name of the chart"
 }
 
 variable "namespace" {
-  type = string
+  type        = string
   description = "Namespace to install the chart"
 }
 
 variable "create_namespace" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Create the namespace if it does not exist. Defaults to false"
 }
 
 variable "repo_name" {
-  type = string
+  type        = string
   description = "Name of the Helm repository"
 }
 
 variable "repo_url" {
-  type = string
+  type        = string
   description = "URL of the Helm repository"
 }
 
 variable "cluster_ca_certificate" {
-  type = string
+  type        = string
   description = "CA certificate of the cluster"
 }
 
 variable "cluster_endpoint" {
-  type = string
+  type        = string
   description = "Endpoint of the cluster"
 }
 
 variable "token" {
-  type = string
+  type        = string
   description = "Token to authenticate with the cluster"
 }
 
